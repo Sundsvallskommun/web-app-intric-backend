18df2710d3b1bb36e26ede7a0ad50f8c3b8950bc
Henrik Sandström <henrik.sandstrom@sundsvall.se>
2024-01-30 14:58:41 +0100

    Use hash instead of hmac function

--- backend/src/services/hash.service.ts
+++ backend/src/services/hash.service.ts
@@ -1,6 +1,6 @@
 import { INTRIC_SALT } from '@/config';
 import 'crypto';
-import { createHmac } from 'crypto';
+import { createHash, createHmac } from 'crypto';
 
 export const verifyHash = (user: string, assistant_id: string, hash: string) => {
   if (typeof user !== 'string') {
@@ -18,10 +18,11 @@ export const verifyHash = (user: string, assistant_id: string, hash: string) => 
   console.log('inbound hash:', hash);
   const salt = INTRIC_SALT;
   console.log('using salt:', salt);
-  const input = user + assistant_id;
+  const input = `${user}${assistant_id}${salt}`;
   console.log('Using input:');
   console.log(input);
-  const _hash = createHmac('sha256', salt).update(input).digest('base64');
+  // const _hash = createHmac('sha256', salt).update(input).digest('base64');
+  const _hash = createHash('sha256').update(input).digest('base64');
   console.log('calculated hash:', _hash);
   return typeof hash !== 'undefined' && typeof _hash !== 'undefined' && _hash === hash;
 };
