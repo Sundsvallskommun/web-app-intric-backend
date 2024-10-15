d29a97ffb708155b59c4e1372b1cb225a60a5991
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2024-10-15 09:06:27 +0200

    clean up after rebase

--- backend/src/services/intric-api-key.service.ts
+++ backend/src/services/intric-api-key.service.ts
@@ -15,7 +15,6 @@ import {
 } from '@/config';
 import { Request } from 'express';
 import prisma from '../utils/prisma';
-import { INTRIC_APIKEY_PRATOMAT1, INTRIC_APIKEY_PRATOMAT2, INTRIC_APIKEY_PRATOMAT3, INTRIC_APIKEY_PRATOMAT4, INTRIC_APIKEY_PRATOMAT5, INTRIC_APIKEY_PRATOMAT6, INTRIC_APIKEY_PRATOMAT7, INTRIC_APIKEY_PRATOMAT8, INTRIC_APIKEY_QWERTY, INTRIC_APIKEY_SERVANET, INTRIC_APIKEY_VUX, INTRIC_APIKEY_WISSER } from '@/config';
 
 export const getApiKey = async (req: Request) => {
   const app = req.headers['_skapp'] ? (req.headers['_skapp'] as string) : undefined;
@@ -29,10 +28,9 @@ export const getApiKey = async (req: Request) => {
     const assistant = await prisma.assistant.findUnique({ where: { app } });
     return assistant.apiKey;
   } catch (err) {
-    console.log("Not in database, looking in .env");
+    console.log('Not in database, looking in .env');
   }
 
- 
   switch (app) {
     case 'PRATOMAT1':
       console.log('Returning api key for PRATOMAT1');
