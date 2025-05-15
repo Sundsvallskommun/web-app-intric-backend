172baf78f3fd4738fcc150b6dfa8d205370fe6b1
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2025-05-15 16:38:19 +0200

    feat: health contract and swagger

--- backend/src/controllers/health.controller.ts
+++ backend/src/controllers/health.controller.ts
@@ -1,7 +1,9 @@
+import { HealthCheckStatus } from '@/responses/health.controller';
 import ApiService from '@/services/api.service';
 import { logger } from '@/utils/logger';
-import { Controller, Get } from 'routing-controllers';
-import { OpenAPI } from 'routing-controllers-openapi';
+import { Response } from 'express';
+import { Controller, Get, HttpCode, HttpError, Res } from 'routing-controllers';
+import { OpenAPI, ResponseSchema } from 'routing-controllers-openapi';
 
 @Controller()
 export class HealthController {
@@ -9,16 +11,18 @@ export class HealthController {
 
   @Get('/health/up')
   @OpenAPI({ summary: 'Return health check' })
-  async up() {
+  @ResponseSchema(HealthCheckStatus)
+  async up(@Res() response: Response<Record<string, string>>): Promise<Response<Record<string, string>>> {
     const url = `simulatorserver/2.0/simulations/response?status=200%20OK`;
     const data = {
       status: 'OK',
     };
-    const res = await this.apiService.post<{ status: string }>({ url, data }).catch(e => {
+    try {
+      const res = await this.apiService.post<Record<string, string>>({ url, data });
+      return response.send(res.data);
+    } catch (e) {
       logger.error('Error when doing health check:', e);
-      return e;
-    });
-
-    return res.data;
+      throw new HttpError(e?.httpCode ?? 500, e?.message ?? 'Error when doing health check');
+    }
   }
 }
