1368c0c064ef9d5031e837379a4d2ec3af06c6cb
Jerker Osatie <jerker.haggstrom.osatie@sundsvall.se>
2025-05-15 16:02:18 +0200

    feat: swagger and validation for info blobs

--- backend/src/controllers/info-blob.controller.ts
+++ backend/src/controllers/info-blob.controller.ts
@@ -1,56 +1,112 @@
-import { InfoBlobPublic, InfoBlobUpdatePublic, PaginatedResponseInfoBlobPublicNoText } from '@/data-contracts/intric/data-contracts';
+import {
+  InfoBlobPublic as InfoBlobPublicInterface,
+  InfoBlobUpdatePublic,
+  PaginatedResponseInfoBlobPublicNoText as PaginatedResponseInfoBlobPublicNoTextInterface,
+} from '@/data-contracts/intric/data-contracts';
 import { UpdateInfoBlobDto } from '@/dtos/info-blob.dto';
 import applicationModeMiddleware from '@/middlewares/application-mode.middleware';
+import hashMiddleware from '@/middlewares/hash.middleware';
 import { validationMiddleware } from '@/middlewares/validation.middleware';
-import ApiService from '@/services/api.service';
+import { InfoBlobPublic, PaginatedResponseInfoBlobPublicNoText } from '@/responses/intric/info-blob.response';
+import { getApiKey } from '@/services/intric-api-key.service';
 import IntricApiService from '@/services/intric-api.service';
-import { Body, Controller, Delete, Get, Param, Post, UseBefore } from 'routing-controllers';
-import { OpenAPI } from 'routing-controllers-openapi';
+import { logger } from '@/utils/logger';
+import { Request, Response } from 'express';
+import { Body, Controller, Delete, Get, HttpError, Param, Post, Req, Res, UseBefore } from 'routing-controllers';
+import { OpenAPI, ResponseSchema } from 'routing-controllers-openapi';
 
 @UseBefore(applicationModeMiddleware)
+@UseBefore(hashMiddleware)
 @Controller()
 export class InfoBlobController {
-  private apiService = new ApiService();
   private intricApiService = new IntricApiService();
 
   @Get('/info-blobs')
   @OpenAPI({
     summary: 'Get info blobs',
   })
-  async get_infoblobs(): Promise<PaginatedResponseInfoBlobPublicNoText> {
+  @ResponseSchema(PaginatedResponseInfoBlobPublicNoText)
+  async get_infoblobs(
+    @Req() req: Request,
+    @Res() response: Response<PaginatedResponseInfoBlobPublicNoTextInterface>,
+  ): Promise<Response<PaginatedResponseInfoBlobPublicNoTextInterface>> {
     const url = '/info-blobs/';
-    const res = await this.intricApiService.get<PaginatedResponseInfoBlobPublicNoText>({ url });
-    return res.data;
+    const apiKey = await getApiKey(req);
+    try {
+      const res = await this.intricApiService.get<PaginatedResponseInfoBlobPublicNoTextInterface>({ url, headers: { 'api-key': apiKey } });
+      return response.send(res.data);
+    } catch (e) {
+      logger.error('Error getting info blobs.', e);
+      throw new HttpError(e?.httpCode ?? 500, e?.message ?? 'Could not get info blobs');
+    }
   }
 
   @Get('/info-blobs/:id')
   @OpenAPI({
     summary: 'Get info blob by id',
   })
-  async get_infoblob_by_id(@Param('id') id: string): Promise<InfoBlobPublic> {
+  @ResponseSchema(InfoBlobPublic)
+  async get_infoblob_by_id(
+    @Req() req: Request,
+    @Param('id') id: string,
+    @Res() response: Response<InfoBlobPublic>,
+  ): Promise<Response<InfoBlobPublicInterface>> {
     const url = `/info-blobs/${id}/`;
-    const res = await this.intricApiService.get<InfoBlobPublic>({ url });
-    return res.data;
+    const apiKey = await getApiKey(req);
+    try {
+      const res = await this.intricApiService.get<InfoBlobPublicInterface>({ url, headers: { 'api-key': apiKey } });
+      return response.send(res.data);
+    } catch (e) {
+      logger.error('Error getting info blob', e);
+      throw new HttpError(e?.httpCode ?? 500, e?.message ?? 'Could not get info blob');
+    }
   }
 
   @Post('/info-blobs/:id')
   @OpenAPI({
     summary: 'Update info blob',
   })
+  @ResponseSchema(InfoBlobPublic)
   @UseBefore(validationMiddleware(UpdateInfoBlobDto, 'body'))
-  async update_infoblob(@Param('id') id: string, @Body() body: UpdateInfoBlobDto): Promise<InfoBlobPublic> {
+  async update_infoblob(
+    @Req() req: Request,
+    @Param('id') id: string,
+    @Body() body: UpdateInfoBlobDto,
+    @Res() response: Response<InfoBlobPublic>,
+  ): Promise<Response<InfoBlobPublicInterface>> {
     const url = `/info-blobs/${id}/`;
-    const res = await this.intricApiService.post<InfoBlobPublic, InfoBlobUpdatePublic>({ url, data: body });
-    return res.data;
+    const apiKey = await getApiKey(req);
+    try {
+      const res = await this.intricApiService.post<InfoBlobPublicInterface, InfoBlobUpdatePublic>({
+        url,
+        data: body,
+        headers: { 'api-key': apiKey },
+      });
+      return response.send(res.data);
+    } catch (e) {
+      logger.error('Error updating info blob', e);
+      throw new HttpError(e?.httpCode ?? 500, e?.message ?? 'Could not update info blob');
+    }
   }
 
   @Delete('/info-blobs/:id')
   @OpenAPI({
     summary: 'Delete info blob',
   })
-  async delete_infoblob(@Param('id') id: string): Promise<InfoBlobPublic> {
+  @ResponseSchema(InfoBlobPublic)
+  async delete_infoblob(
+    @Req() req: Request,
+    @Param('id') id: string,
+    @Res() response: Response<InfoBlobPublic>,
+  ): Promise<Response<InfoBlobPublicInterface>> {
     const url = `/info-blobs/${id}/`;
-    const res = await this.intricApiService.delete<InfoBlobPublic>({ url });
-    return res.data;
+    const apiKey = await getApiKey(req);
+    try {
+      const res = await this.intricApiService.delete<InfoBlobPublicInterface>({ url, headers: { 'api-key': apiKey } });
+      return response.send(res.data);
+    } catch (e) {
+      logger.error('Error deleting info blob', e);
+      throw new HttpError(e?.httpCode ?? 500, e?.message ?? 'Could not delete info blob');
+    }
   }
 }
